dcc58d02694a133a75f3e609115ea35ac799e458
muhammadjonr <mrahmonaliev22032007@gmail.com>
2023-10-25 12:59:33 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+let menu = document.getElementById("burger_menu");
+
+menu.addEventListener("click", function () {
+  document.body.classList.toggle("open_menu");
+});
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
